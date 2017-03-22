e14ed08cc3f8b14bc7f28b03084249efbfb4422d
chizhonghao <chi@192.168.0.100>
2017-03-22 21:38:57 +0800

    新项目FM

---
+++ FM/FM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FM
+//
+//  Created by chizhonghao on 17/3/22.
+//  Copyright © 2017年 chizhonghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
